0b9a15693d66fec0fc78e90890402ff47cd38c9c
likaannisa <likanurannisa@gmail.com>
2020-02-13 15:46:35 +0700

    name checkbox

--- resources/views/mitra/ngo/tenaga/tenaga.blade.php
+++ resources/views/mitra/ngo/tenaga/tenaga.blade.php
@@ -66,11 +66,11 @@
                                       <td><a href="/tenaga/{{$ten -> id}}/edit"  class="btn btn-sm btn-primary"><i class="fas fa-edit"></i> Ubah</a></td>
                                       <td>
                                       <div class="icheck-success d-inline">
-                                          <input type="checkbox" id="{{ $ten->id }}">
+                                          <input type="checkbox" name="tenaga[]" value="{{ $ten->id }}">
                                           <label for="{{ $ten->id }}"></label>
                                       </div>
                                       </td>
-                                  </tr>  
+                                  </tr>
                                   @endforeach
                                 </tbody>
                             </table>
@@ -86,9 +86,9 @@
                 </div>
             </div>
         </div>
-    </section>   
+    </section>
 </div>
-  
+
 @endsection
 
 
