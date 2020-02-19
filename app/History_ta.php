8bfb5e21965adb1b27caefa73f98caec2a90d9d5
Alfine_putra <alfineputra7@gmail.com>
2020-02-19 11:01:19 +0700

    update_history

---
+++ app/History_ta.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class History_ta extends Model
+{
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama',
+'kewarganegaraan','no_passport',
+'tgl_berlaku_awal','tgl_berlaku_akhir',
+'id_instansi','jabatan','foto','upload_passpor',
+'cv_resume','jenis_kelamin','tempat_lahir','tanggal_lahir',
+'kategori','tujuan','kegiatan','tgl_awal','tgl_akhir','foto',
+'upload_passport','jobdesc','dokumen_pendukung','status','status_keaktifan','file_perpanjangan','catatan','created_at','updated_at'
+    ];
+}
