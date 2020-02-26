b6dd3d2ba23aec412bfeb81be4638c2a0a65891c
farzet90 <farhanzlkr9@gmail.com>
2020-02-26 10:27:05 +0700

    Update register.blade.php

--- resources/views/dashboard/register.blade.php
+++ resources/views/dashboard/register.blade.php
@@ -69,6 +69,12 @@
                                     <input type="email" class="form-control" name="email_user" id="email-user" required>
                                 </div>
                                 <div class="form-group">
+                                    <h6>Nama Lengkap</h6>
+                                    <input type="text" class="form-control" name="name_user" id="name_user" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                <div class="form-group">
                                     <h6>Password</h6>
                                     <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" required>
                                     @error('password')
@@ -81,23 +87,9 @@
                                     <h6>Konfirmasi Password</h6>
                                     <input type="password" class="form-control" name="password_confirmation" id="password-confirm" required>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                <div class="form-group">
-                                    <h6>Nama Lengkap</h6>
-                                    <input type="text" class="form-control" name="name_user" id="name_user" required>
-                                </div>
-                                <div class="form-group">
-                                    <h6>Country Director/Representative</h6>
-                                    <input type="text" class="form-control" name="country_director" id="country_director" required>
-                                </div>
-                                <div class="form-group">
-                                    <h6>Instansi</h6>
-                                    <input type="text" class="form-control" name="instansi" id="instansi">
-                                </div>
                                 <div class="form-group">
                                     <h6>Nomor Telepon</h6>
-                                    <input type="number" class="form-control" name="telp_user" id="telp-user">
+                                    <input type="tel" class="form-control" name="telp_user" id="telp-user">
                                 </div>
                             </div>
                         </div>
