eb8ad3dc423215e59b981abafdc8a55b48f5ad2f
alifjunjunan <junjunanalif@gmail.com>
2020-04-12 14:55:36 +0700

    sugoiii nee

--- app/Http/Controllers/Verif_rikController.php
+++ app/Http/Controllers/Verif_rikController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Master_program;
+use App\Rik_kegiatan;
 use Illuminate\Http\Request;
 
 class Verif_rikController extends Controller
 {
  public function index(){
-     $program = Master_program::where('status','0')->latest()->paginate(5);
+     $program = Master_program::paginate(5);
 
-     return view('mitra.Fasker.RIK.master_rik',['program',$program]);
+     $kegiatan = Rik_kegiatan::where('status','0')->latest()->paginate(5);
+
+     return view('mitra.Fasker.RIK.master_rik',['program'=>$program,'kegiatan'=>$kegiatan]);
  }
 }
